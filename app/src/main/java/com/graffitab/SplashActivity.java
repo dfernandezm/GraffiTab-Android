b027a321f6583c7d271300c5767e638c00d5dcad
Georgi Christov <georgi.christov89@gmail.com>
2016-07-04 04:14:22 +0100

    Initial SDK configuration.

--- app/src/main/java/com/graffitab/SplashActivity.java
+++ app/src/main/java/com/graffitab/SplashActivity.java
@@ -3,6 +3,9 @@ package com.graffitab;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.graffitab.graffitabsdk.config.GTConfig;
+import com.graffitab.graffitabsdk.config.GTSDKConfig;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
@@ -10,5 +13,16 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_splash);
+
+        setupSDK();
+    }
+
+    // Setup
+
+    private void setupSDK() {
+        GTConfig config = GTConfig.defaultConfig();
+        config.logEnabled = true;
+
+        GTSDKConfig.sharedInstance.setConfig(config);
     }
 }
