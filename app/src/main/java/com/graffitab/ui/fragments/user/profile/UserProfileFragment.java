d65f85ec0169d2b3af2d0c5f6957c198123b6f37
Georgi Christov <georgi.christov89@gmail.com>
2016-11-22 02:24:00 +0000

    First version of user profile screen.

--- app/src/main/java/com/graffitab/ui/fragments/user/profile/UserProfileFragment.java
+++ app/src/main/java/com/graffitab/ui/fragments/user/profile/UserProfileFragment.java
@@ -1,12 +1,17 @@
 package com.graffitab.ui.fragments.user.profile;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.graffitab.R;
 import com.graffitab.ui.fragments.streamable.ListStreamablesFragment;
-import com.graffitab.utils.display.DisplayUtils;
 
 /**
  * Created by georgichristov on 21/11/2016
@@ -14,6 +19,20 @@ import com.graffitab.utils.display.DisplayUtils;
  * Copyright © GraffiTab Inc. 2016
  */
 public class UserProfileFragment extends ListStreamablesFragment {
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+//        inflater.inflate(R.menu.menu_refresh, menu);
+
+        for (int i = 0; i < menu.size(); i++) {
+            MenuItem menuItem = menu.getItem(i);
+            Drawable drawable = menuItem.getIcon();
+            if (drawable != null) {
+                drawable.mutate();
+                drawable.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+            }
+        }
+    }
 
     @Override
     public RecyclerView.ItemDecoration getItemDecoration() {
@@ -28,7 +47,7 @@ public class UserProfileFragment extends ListStreamablesFragment {
                     outRect.top = 0;
                     outRect.left = 0;
                     outRect.right = 0;
-                    outRect.bottom = DisplayUtils.pxToDip(getActivity(), 5);
+                    outRect.bottom = -7;
                 }
                 else
                     UserProfileFragment.super.getItemDecoration().getItemOffsets(outRect, view, parent, state);
