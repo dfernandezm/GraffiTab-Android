01af388c9b901da2085930369fc892519a87e188
Georgi Christov <georgi.christov89@gmail.com>
2017-02-22 17:53:04 +0000

    Tablet support for comments.

--- app/src/main/java/com/graffitab/ui/activities/home/streamables/CommentsActivity.java
+++ app/src/main/java/com/graffitab/ui/activities/home/streamables/CommentsActivity.java
@@ -2,7 +2,9 @@ package com.graffitab.ui.activities.home.streamables;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 
+import com.graffitab.R;
 import com.graffitab.constants.Constants;
 import com.graffitab.ui.activities.custom.comments.BaseCommentsActivity;
 import com.graffitab.ui.fragments.comments.GenericCommentsFragment;
@@ -10,12 +12,16 @@ import com.graffitab.ui.fragments.comments.ListCommentsFragment;
 import com.graffitab.utils.input.KeyboardUtils;
 import com.graffitabsdk.model.GTStreamable;
 
+import butterknife.BindView;
+
 /**
  * Created by georgichristov on 09/12/2016
  * --
  * Copyright © GraffiTab Inc. 2016
  */
 public class CommentsActivity extends BaseCommentsActivity {
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
 
     private GTStreamable streamable;
 
@@ -34,6 +40,11 @@ public class CommentsActivity extends BaseCommentsActivity {
     }
 
     @Override
+    public int getLayoutResId() {
+        return R.layout.activity_dialog_fragment_holder;
+    }
+
+    @Override
     protected void onPause() {
         KeyboardUtils.hideKeyboard(this);
         super.onPause();
@@ -44,5 +55,14 @@ public class CommentsActivity extends BaseCommentsActivity {
         GenericCommentsFragment fragment = new ListCommentsFragment();
         fragment.setStreamable(streamable);
         return fragment;
+    }
+
+    // Setup
+
+    @Override
+    public void setupTopBar() {
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorPrimary));
+        setSupportActionBar(toolbar);
+        super.setupTopBar();
     }
 }
